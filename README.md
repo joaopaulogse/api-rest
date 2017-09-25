6f9ab6383f813cf8f63802b31c7abde421ce0181
Joao Paulo <joaopaulogse@gmail.com>
2017-09-25 09:54:15 -0300

    aumento na cobertura de testes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # API
 
+[![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest)
+
 > REST API With Nodejs, Express, Sequelize, Passportjs, Dotenv-save and bcript using Docker
--- src/api/tests/integration/routes/user.spec.js
+++ src/api/tests/integration/routes/user.spec.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const request = require("supertest");
 const server = require("../../../../server");
 
-describe('server test', () => {
+describe('Servidor de Test de Integração', () => {
 
     describe("GET /users", () => {
         it("Lista os usuarios", done => {
@@ -15,7 +15,7 @@ describe('server test', () => {
         })
     });
     let id = null
-    describe('Cadastro de usuario /cadastro', ()=>{
+    describe('Cadastro de usuario, POST /cadastro', ()=>{
         it('Cadastra usuario e em logo em seguida exclui', done=>{
             let data = {
                 username:'joaoTest',
@@ -41,7 +41,37 @@ describe('server test', () => {
                 .then(response => {
                     expect(response.statusCode).equal(200);
                     done();
-                })
-        })
-    })  
+                });
+        });
+    })  ;
+
+    describe(`PUT /users/:id`, ()=>{
+        it('Alterar Usuario', done=>{
+            let dataUpdate = {
+                username:'joaoTest2_ALTEREI',
+                password:'1234567890',
+                tipo:'ADMIN',
+                email:'joaoTest2@testIntegration.com'
+            }
+            // global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+            request(server)
+                .put(`/users/${id}`)
+                .send(dataUpdate)
+                .then(response=>{
+                    expect(response.statusCode).equal(201)
+                    done()
+                });
+        });
+    });
+
+    describe('DELETE /USER/:id', ()=>{
+        it('Delete Usuario', done=>{
+            request(server)
+                .delete(`/users/${id}`)
+                .then(response=>{
+                    expect(response.statusCode).equal(201);
+                    done();
+                });
+        });
+    });
 })
