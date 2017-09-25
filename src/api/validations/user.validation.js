9967bdf753cd63aa8e9521ce66ecafb89d189fb0
Joao Paulo <joaopaulogse@gmail.com>
2017-09-25 17:57:12 -0300

    melhorias na validação

--- src/api/validations/user.validation.js
+++ src/api/validations/user.validation.js
@@ -9,15 +9,5 @@ module.exports = {
             email: Joi.string().email().required(),
             tipo: Joi.strict()
         }
-    },
-    updateValidation:{
-        body: {
-            username: Joi.string().min(3).required(),
-            password: Joi.string().min(6).max(20).required(),
-            email: Joi.string().email().required(),
-            tipo: Joi.strict()
-        }
     }
-
-
 }
