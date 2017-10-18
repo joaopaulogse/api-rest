a3a51677a09c76267bc3d691ee1f76aaadbfb102
Joao Paulo <joaopaulogse@gmail.com>
2017-10-18 16:16:22 -0200

    graphql with mongodb

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -3,11 +3,16 @@ const validate = require("express-validation");
 const { create } = require("../controllers/user.controller");
 const { registrar } = require("../validations/user.validation");
 const routerUser = require("./user.routes");
+const graphqlHTTP = require('express-graphql')
+const { schema } = require('../models/UserSchema');
 
 const router = express.Router();
 
-
 router.use("/users", routerUser);
+router.use('/graphql', graphqlHTTP({
+    schema:schema,
+    graphiql:true
+}));
 router.post("/cadastro", validate(registrar), create);
 
 module.exports = router;
